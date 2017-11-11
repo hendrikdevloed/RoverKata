66d1b4f3a21be3eeef9a9616b4cf652a705ccc87
Hendrik De Vloed <hendrik.de.vloed@gmail.com>
2017-11-11 18:30:21 +0100

    xUnit testing and property based

---
+++ RoverKataTest/RoverTest.fs
@@ -0,0 +1,18 @@
+﻿module RoverTest
+
+open Xunit
+open FsCheck.Xunit
+open Rover
+
+[<Fact>]
+let test() = ()
+
+[<Property>]
+let ``Three rights make a left``(rover:Rover) =
+    rover |> turnRight |> turnRight|> turnRight = turnLeft rover
+
+
+[<Property>]
+let ``Four rights is nop``(rover:Rover) =
+    rover |> turnRight |> turnRight |> turnRight|> turnRight = rover
+

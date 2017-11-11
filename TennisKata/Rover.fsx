2d44155323be912889f8b05864482a6e308a4e1e
Hendrik De Vloed <hendrik.de.vloed@gmail.com>
2017-11-11 15:11:59 +0100

    rotating

--- TennisKata/Rover.fsx
+++ TennisKata/Rover.fsx
@@ -28,19 +28,43 @@ let add p1 = fun p2 -> {
 let moveForward rover = {rover with location = add rover.location (step rover.facing) }
 let moveBackward rover = {rover with location = add rover.location (opposite (step rover.facing)) }
 
-let move rover towards =
+let rightOf direction =
+    match direction with
+    | N -> E
+    | E -> S
+    | S -> W
+    | W -> N
+
+let leftOf direction =
+    match direction with
+    | N -> W
+    | W -> S
+    | S -> E
+    | E -> N
+
+let turnLeft rover = {rover with facing=leftOf rover.facing}
+let turnRight rover = {rover with facing=rightOf rover.facing}
+
+let move towards =
     match towards with
-    | F ->  moveForward rover
-    | B ->  moveBackward rover
-    | _ -> id rover
+    | F ->  moveForward
+    | B ->  moveBackward
+    | L -> turnLeft
+    | R -> turnRight
 
 let moveForwardsOne() =
-    move {location={x=10;y=10}; facing=N} F = {location={x=10;y=9}; facing=N}
+    (move F) {location={x=10;y=10}; facing=N} = {location={x=10;y=9}; facing=N}
 
 let moveBackwardsOne() =
-    move {location={x=10;y=10}; facing=N} B = {location={x=10;y=11}; facing=N}
+    (move B) {location={x=10;y=10}; facing=N} = {location={x=10;y=11}; facing=N}
+
+let leftOfTest() =
+    leftOf N = W
+    && leftOf S = E
 
 let test =
-    moveForwardsOne()
+    leftOfTest()
+    && moveForwardsOne()
     && moveBackwardsOne()
     && true
+
